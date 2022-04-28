f11bc7c1d4b9299e95238635bd57d0986d7eb478
Harold Barron <haroldbarron13@gmail.com>
2022-04-27 22:27:49 -0500

    Creating method getExplorersUsernamesByMission in ExplorerController.js

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -2,9 +2,17 @@ const ExplorerController = require ("./../../lib/controllers/ExplorerController"
 
 describe("Test for Explorer Controller", ()=>
 {
-    test("1.",()=>
+    test("1. Gettin de list of explorers with a specific mission",()=>
     {
         const controller = ExplorerController.getExplorersByMission("node")
+        const ExplorersWithNode = controller.forEach(expName => expect(expName.mission).toContain("node"));
+    })
+    test("2. Gettin de names of the explorers in a specific mission",()=>
+    {
+        const controller = ExplorerController.getExplorersUsernamesByMission("node")
+        console.log(controller)
         
     })
 })
+
+
